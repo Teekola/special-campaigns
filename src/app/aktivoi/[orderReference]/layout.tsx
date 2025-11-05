7c4a04a579473714ad142316f4341c02090f96bd
Teemu Kerkola <tokerk@utu.fi>
2025-11-05 08:37:33 +0200

    feat: only border bottom

--- src/app/aktivoi/[orderReference]/layout.tsx
+++ src/app/aktivoi/[orderReference]/layout.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from "react";
 export default function Layout({ children }: PropsWithChildren) {
    return (
       <div className="mx-auto w-full space-y-12">
-         <div className="h-20 flex items-center justify-center border">
+         <div className="h-20 flex items-center justify-center border-b">
             <p className="font-bold">Black Friday 2025</p>
          </div>
          <div className="p-4">{children}</div>
